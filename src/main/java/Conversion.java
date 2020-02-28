7b95167a4879342073f1a6892807ea9111ed0764
radiales <radiales1@gmail.com>
2020-02-28 23:38:10 +0100

    First tries

--- src/main/java/Conversion.java
+++ src/main/java/Conversion.java
@@ -1,7 +1,68 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import freemarker.template.*;
+
 public class Conversion {
 
-    public static void main(String[] args){
+    private static Object Version;
 
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException, TemplateException {
+
+        // 1. Configure FreeMarker
+        //
+        // You should do this ONLY ONCE, when your application starts,
+        // then reuse the same Configuration object elsewhere.
+
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+
+        // Where do we load the templates from:
+        cfg.setClassForTemplateLoading(ValueAnswersObject.class, "templates");
+
+        // Some other recommended settings:
+        cfg.setIncompatibleImprovements(new Version(2, 3, 29));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setLocale(Locale.GERMAN);
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        // 2. Proccess template(s)
+        //
+        // You will do this for several times in typical applications.
+
+        // 2.1. Prepare the template input:
+
+        Map<String, Object> input = new HashMap<String, Object>();
+
+        input.put("title", "radiales example");
+
+        input.put("answerObject", new ValueAnswersObject("Java object", "me", "Yolo"));
+
+        List<ValueAnswersObject> systems = new ArrayList<ValueAnswersObject>();
+        systems.add(new ValueAnswersObject("Android", "Google", "Google"));
+        input.put("systems", systems);
+
+        // 2.2. Get the template
+
+        Template template = cfg.getTemplate("conv.ftl");
+
+        // 2.3. Generate the output
+
+        // Write output to the console
+        Writer consoleWriter = new OutputStreamWriter(System.out);
+        template.process(input, consoleWriter);
+
+        // For the sake of example, also write output into a file:
+        Writer fileWriter = new FileWriter(new File("output.html"));
+        try {
+            template.process(input, fileWriter);
+        } finally {
+            fileWriter.close();
+        }
+
     }
-}
+    }
+
---
+++ src/main/java/ValueAnswersObject.java
@@ -0,0 +1,25 @@
+public class ValueAnswersObject {
+
+    private String  question;
+    private String  answer;
+    private String  category;
+
+
+    public ValueAnswersObject(String q, String a, String c){                                                            //Standard Konstruktor, Die Strings müssen Simple Strings sein für die Tabelle
+        this.question   = new String(q);
+        this.answer     = new String(a);
+        this.category   = new String(c);
+
+    }
+
+    /*===============================| Getter / Setter |===============================*/
+    public String   getQuestion()           {return this.question;}
+    public void     setQuestion(String s)   {this.question = new String(s);}
+
+    public String   getCategory()           {return this.category;}
+    public void     setCategory(String s)   {this.category = new String(s);}
+
+    public String   getAnswer()             {return this.answer;}
+    public void     setAnswer(String s)     {this.answer = new String(s);}
+    /*=================================================================================*/
+}
